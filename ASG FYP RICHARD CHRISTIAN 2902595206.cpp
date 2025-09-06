82ce6d2c7ff3a3f9071c16cc690f00bf4ebd6a62
RCHRRRRR <richard.christian002@binus.ac.id>
2025-09-06 21:17:41 +0700

    Add files via upload

---
+++ ASG FYP RICHARD CHRISTIAN 2902595206.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Nama saya Richard Christian");
+    printf("Bahasa Pemograman Favorit Saya adalah C++");
+
+    return 0;
+}
